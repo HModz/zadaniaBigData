c8be04703a7f130c8bc89f458860a9b665de43c1
HModz <modzel12345@o2.pl>
2020-06-03 19:35:40 +0200

    binarne

--- 17-binarne.py
+++ 17-binarne.py
@@ -1,10 +1,10 @@
 # przekształcanie liczb na system binarny
 
 liczba = int(input("Podaj liczbę: "))
-dwa = 2
+
 def Binarna(x):
         if x > 0:
             Binarna(x // 2)
             print(x % 2, end="")
 
-Binarna(x=liczba)
+Binarna(liczba)
